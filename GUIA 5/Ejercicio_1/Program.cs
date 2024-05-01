34cc9ca5ee0488ec7db1b7e581233ffa6dff8142
Renzoh17 <hirschfeldrenzo@gmail.com>
2024-04-30 21:13:21 -0300

    ejercicio1

--- GUIA 5/Ejercicio_1/Program.cs
+++ GUIA 5/Ejercicio_1/Program.cs
@@ -10,7 +10,33 @@ namespace Ejercicio_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola Mundo");
+            string nomMayor = "", nomMenor = "";
+            float montoMenor=0, montoMayor=0;
+            Console.WriteLine("Ingrese el nombre y el monto del apostador");
+            string nom= Console.ReadLine();
+            float monto= Convert.ToSingle(Console.ReadLine());
+            montoMayor = monto;
+            montoMenor = monto;
+            nomMayor = nom;
+            nomMenor= nom;
+            while(monto>0)
+            {
+                if (monto < montoMenor)
+                {
+                    montoMenor= monto;
+                    nomMenor=nom;
+                }
+                if (monto>montoMayor) 
+                {
+                    montoMayor= monto;
+                    nomMayor= nom;
+                }
+                Console.WriteLine("Ingrese el nombre y el monto del apostador");
+                 nom = Console.ReadLine();
+                 monto = Convert.ToSingle(Console.ReadLine());
+            }
+            Console.WriteLine("el monto mayor es {0},{1}",montoMayor,nomMayor);
+            Console.WriteLine("el monto menor es {0},{1}", montoMenor, nomMenor);
             Console.ReadKey();
         }
     }

455da0e52d61bfbd9e472b1bda549c07b2e939de
Renzoh17 <hirschfeldrenzo@gmail.com>
2024-06-04 21:29:10 -0300

    Clase 10

---
+++ Clase 11/Formularios/Prueba/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace Prueba
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAgregarNum = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tbIngresarNum = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lbProm = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.rbBinaria = new System.Windows.Forms.RadioButton();
+            this.rbSecuencial = new System.Windows.Forms.RadioButton();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.tbBusqueda = new System.Windows.Forms.TextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnListar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnAgregarNum
+            // 
+            this.btnAgregarNum.Location = new System.Drawing.Point(354, 30);
+            this.btnAgregarNum.Name = "btnAgregarNum";
+            this.btnAgregarNum.Size = new System.Drawing.Size(75, 55);
+            this.btnAgregarNum.TabIndex = 0;
+            this.btnAgregarNum.Text = "Agregar Número";
+            this.btnAgregarNum.UseVisualStyleBackColor = true;
+            this.btnAgregarNum.Click += new System.EventHandler(this.btnAgregarNum_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tbIngresarNum);
+            this.groupBox1.Controls.Add(this.btnAgregarNum);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(483, 110);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ingresar Número";
+            // 
+            // tbIngresarNum
+            // 
+            this.tbIngresarNum.Location = new System.Drawing.Point(63, 48);
+            this.tbIngresarNum.Name = "tbIngresarNum";
+            this.tbIngresarNum.Size = new System.Drawing.Size(100, 20);
+            this.tbIngresarNum.TabIndex = 1;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(354, 31);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 50);
+            this.btnCalcular.TabIndex = 3;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lbProm);
+            this.groupBox2.Controls.Add(this.btnCalcular);
+            this.groupBox2.Location = new System.Drawing.Point(12, 133);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(483, 100);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Mostrar Promedio";
+            // 
+            // lbProm
+            // 
+            this.lbProm.AutoSize = true;
+            this.lbProm.Location = new System.Drawing.Point(60, 50);
+            this.lbProm.Name = "lbProm";
+            this.lbProm.Size = new System.Drawing.Size(60, 13);
+            this.lbProm.TabIndex = 4;
+            this.lbProm.Text = "Promedio: -";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.rbBinaria);
+            this.groupBox3.Controls.Add(this.rbSecuencial);
+            this.groupBox3.Controls.Add(this.btnBuscar);
+            this.groupBox3.Controls.Add(this.tbBusqueda);
+            this.groupBox3.Location = new System.Drawing.Point(12, 239);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(483, 164);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Busqueda de Número";
+            // 
+            // rbBinaria
+            // 
+            this.rbBinaria.AutoSize = true;
+            this.rbBinaria.Location = new System.Drawing.Point(63, 119);
+            this.rbBinaria.Name = "rbBinaria";
+            this.rbBinaria.Size = new System.Drawing.Size(57, 17);
+            this.rbBinaria.TabIndex = 3;
+            this.rbBinaria.TabStop = true;
+            this.rbBinaria.Text = "Binaria";
+            this.rbBinaria.UseVisualStyleBackColor = true;
+            // 
+            // rbSecuencial
+            // 
+            this.rbSecuencial.AutoSize = true;
+            this.rbSecuencial.Location = new System.Drawing.Point(63, 96);
+            this.rbSecuencial.Name = "rbSecuencial";
+            this.rbSecuencial.Size = new System.Drawing.Size(78, 17);
+            this.rbSecuencial.TabIndex = 2;
+            this.rbSecuencial.TabStop = true;
+            this.rbSecuencial.Text = "Secuencial";
+            this.rbSecuencial.UseVisualStyleBackColor = true;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(354, 40);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 1;
+            this.btnBuscar.Text = "Busqueda";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // tbBusqueda
+            // 
+            this.tbBusqueda.Location = new System.Drawing.Point(63, 42);
+            this.tbBusqueda.Name = "tbBusqueda";
+            this.tbBusqueda.Size = new System.Drawing.Size(100, 20);
+            this.tbBusqueda.TabIndex = 0;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.listBox1);
+            this.groupBox4.Controls.Add(this.btnListar);
+            this.groupBox4.Location = new System.Drawing.Point(501, 12);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(231, 391);
+            this.groupBox4.TabIndex = 6;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Lista Ordenada";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(6, 19);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 355);
+            this.listBox1.TabIndex = 1;
+            // 
+            // btnListar
+            // 
+            this.btnListar.Location = new System.Drawing.Point(137, 30);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(75, 23);
+            this.btnListar.TabIndex = 0;
+            this.btnListar.Text = "Listar";
+            this.btnListar.UseVisualStyleBackColor = true;
+            this.btnListar.Click += new System.EventHandler(this.btnListar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1097, 632);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAgregarNum;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox tbIngresarNum;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lbProm;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RadioButton rbSecuencial;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.TextBox tbBusqueda;
+        private System.Windows.Forms.RadioButton rbBinaria;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnListar;
+    }
+}
+
---
+++ Clase 11/Formularios/Prueba/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prueba
+{
+    public partial class Form1 : Form
+    {
+        int[] listnum = new int[100];
+        int cont = 0;
+        float acum = 0;
+        float prom;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnAgregarNum_Click(object sender, EventArgs e)
+        {
+            listnum[cont] = Convert.ToInt32(tbIngresarNum.Text);
+            cont++;
+            tbIngresarNum.Clear();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            lbProm.Text = "Promedio: ";
+            for (int i = 0; i < cont; i++)
+            {
+                acum += listnum[i];
+            }
+            if (acum != 0)
+            {
+            prom = acum / cont;
+            acum = 0;
+            lbProm.Text += $"{prom:f2}";
+            }
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            if (rbSecuencial.Checked == true){
+                BusquedaSecuencial(tbBusqueda.Text);
+            }
+            if (rbBinaria.Checked == true)
+            {
+                BusquedaBinaria(tbBusqueda.Text);
+            }
+            else
+            {
+                MessageBox.Show("Seleccione un metodo de busqueda");
+            }
+        }
+
+        private void BusquedaSecuencial(string tbBus)
+        {
+            bool encontrado = false; ;
+            for (int i = 0; i < cont; i++)
+            {
+                if (listnum[i] == Convert.ToInt32(tbBus)) encontrado = true;
+            }
+            if (encontrado) MessageBox.Show("Valor Encontrado");
+            else MessageBox.Show("Valor No Encontrado");
+        }
+
+        private void BusquedaBinaria(string tbBusq)
+        {
+            int pivot = 0, minl = 0, maxl = cont;
+            bool encontrado = false;
+            OrdenamientoBurbuja();
+            while (minl <= maxl && !encontrado)
+            {
+                pivot = (minl + maxl) / 2;
+                if (listnum[pivot] == Convert.ToInt32(tbBusq))
+                {
+                    encontrado = true;
+                }
+                if (listnum[pivot] > Convert.ToInt32(tbBusq))
+                {
+                    maxl = pivot - 1;
+                }
+                else
+                {
+                    minl = pivot + 1;
+                }
+            }
+            if (!encontrado)
+            {
+                MessageBox.Show("Valor No encontrado");
+            }
+            else
+            {
+                MessageBox.Show("Valor Encontrado");
+            }
+        }
+
+        private void OrdenamientoBurbuja()
+        {
+            int aux;
+            for(int i = 0; i < cont; i++)
+            {
+                for(int j = i + 1; j < cont; j++)
+                {
+                    if (listnum[i] > listnum[j])
+                    {
+                        aux = listnum[i];
+                        listnum[i] = listnum[j];
+                        listnum[j] = aux;
+                    }
+                }
+            }
+        }
+
+        private void btnListar_Click(object sender, EventArgs e)
+        {
+            OrdenamientoBurbuja();
+            listBox1.Items.Clear();
+            for(int i = 0; i < cont; i++)
+            {
+                listBox1.Items.Add(listnum[i]);
+            }
+        }
+    }
+}
